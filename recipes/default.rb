eccf3c8db5e7eb94cf4360456a44b0a05f733ead
Jerry Crabb <jerry@respondr.io>
2014-10-21 17:05:21 -0500

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,5 +25,5 @@ template '/etc/rsyslog.conf' do
     :tags => node['loggly']['tags'].nil? || node['loggly']['tags'].empty? ? '' : "tag=\\\"#{node['loggly']['tags'].join("\\\" tag=\\\"")}\\\"",
     :token => loggly_token
   })
-  notifies :restart, "service[rsyslog]", :immediate
+  notifies :restart, "service[rsyslog]", :immediately
 end
