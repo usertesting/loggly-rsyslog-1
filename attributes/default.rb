8072a22365c8387e5991129fd45368425837bbf3
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2016-03-21 20:31:19 -0600

    Use updated Loggly cert
    The new cert includes the CA bundle, so the separate cert merging is no longer required.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,10 +5,8 @@ default['loggly']['log_dirs'] = []
 
 default['loggly']['tls']['enabled'] = true
 default['loggly']['tls']['cert_path'] = '/etc/rsyslog.d/keys/ca.d'
-default['loggly']['tls']['cert_url'] = 'https://logdog.loggly.com/media/loggly.com.crt'
-default['loggly']['tls']['cert_checksum'] = 'e65a3dd6eafe4c46d7c171f871bf022ab7d4e551'
-default['loggly']['tls']['intermediate_cert_url'] = 'https://certs.starfieldtech.com/repository/sf_bundle.crt'
-default['loggly']['tls']['intermediate_cert_checksum'] = '9f4b50011bdeabda276c9dd08f32f545218ea1b7'
+default['loggly']['tls']['cert_url'] = 'https://logdog.loggly.com/media/logs-01.loggly.com_sha12.crt'
+default['loggly']['tls']['cert_checksum'] = 'b562ae82b54bcb43923290e78949153c0c64910d40b02d2207010bb119147ffc'
 
 default['loggly']['rsyslog']['host'] = 'logs-01.loggly.com'
 default['loggly']['rsyslog']['port'] = node['loggly']['tls']['enabled'] ? 6514 : 514
--- recipes/tls.rb
+++ recipes/tls.rb
@@ -21,32 +21,11 @@ directory cert_path do
   recursive true
 end
 
-loggly_crt_path = "#{Chef::Config['file_cache_path']}/loggly.com.crt"
-sf_bundle_path = "#{Chef::Config['file_cache_path']}/sf_bundle.crt"
-
-remote_file 'download loggly.com cert' do
+remote_file 'loggly cert' do
   owner 'root'
   group 'root'
   mode 0644
-  path loggly_crt_path
+  path ::File.join(cert_path, 'loggly_full.crt')
   source node['loggly']['tls']['cert_url']
   checksum node['loggly']['tls']['cert_checksum']
-end
-
-remote_file 'download intermediate cert' do
-  owner 'root'
-  group 'root'
-  mode 0644
-  path sf_bundle_path
-  source node['loggly']['tls']['intermediate_cert_url']
-  checksum node['loggly']['tls']['intermediate_cert_checksum']
-end
-  
-bash 'bundle certificate' do
-  user 'root'
-  cwd cert_path
-  code <<-EOH
-    cat {#{sf_bundle_path},#{loggly_crt_path}} > loggly_full.crt
-  EOH
-  not_if { ::File.exists?("#{node['loggly']['tls']['cert_path']}/loggly_full.crt") }
 end
